0905c29fd840d663261a351ec37965247dd151d8
Katie Starrett <starrett.k.e@gmail.com>
2017-07-27 16:18:34 -0400

    1st rough draft

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,8 +6,27 @@ let apiRoutes = {
 	},
 	postRequest: function(req, res) {
 		let newFriend = req.body;
+		let friendCompare = [];
+
+		for (var i = 0; i < allFriends.length; i++) {
+			let totalScore = 0;
+			for (var j = 0; j < allFriends[i].scores.length; j++) {
+				let score = newFriend.scores[j] - parseInt(allFriends[i].scores[j]);
+				totalScore += Math.abs(score);
+			};
+			let friend = {
+				name: allFriends[i].name,
+				photo: allFriends[i].photo,
+				difference: totalScore
+			};
+			friendCompare.push(friend);
+		};
+
+		friendCompare.sort( (a, b) => {
+			return a.difference - b.difference;
+		});
 		allFriends.push(newFriend);
-		res.json(true);
+		res.json(friendCompare[0]);
 	}
 };
 
